4754ca4f05ec564c0c2aaa6013955eca043224f0
alex <alejandrosilvarodriguez9@gmail.com>
2024-04-27 20:04:06 +0200

    breast cancer project added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import diabetes from "../../Assets/Projects/diabetes.jpg";
-import emotion from "../../Assets/Projects/suicide.png";
+import breast_cancer from "../../Assets/Projects/breast_cancer_project.jpeg";
 
 function Projects() {
   return (
@@ -27,11 +27,18 @@ function Projects() {
               ghLink="https://github.com/AlexSilvaa9/Diabetes_prediction_scikitlearn"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
-            <a href="http://www.freepik.com">Designed by vectorjuice / Freepik</a>
-
           </Col>
-
-         
+          <Col md={4} className="project-card">
+            <ProjectCard
+                imgPath={breast_cancer}
+                
+                isBlog={false}
+                title="Breast Cancer Analisys and Prediction"
+                description="The project showcases a systematic approach to developing predictive models in biomedicine, specifically targeting the estimation of metastasis probability in cancer patients. Through data cleaning, statistical analysis, and model refinement techniques, the project demonstrates the potential for enhancing cancer prognosis and treatment efficacy. However, it also highlights the challenges associated with model validation and performance evaluation in biomedical contexts. Overall, the project contributes to the ongoing efforts to leverage predictive modeling for improving patient outcomes in oncology."
+                ghLink="https://github.com/AlexSilvaa9/BreastCancerPrediction"
+                // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              />
+          </Col>
         </Row>
       </Container>
     </Container>
